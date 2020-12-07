6a9b3b7509dec2b77dfb13b30466e6ca4162097d
Chris Bailey <chris@ghostly.io>
2020-12-06 23:00:03 -0500

    feat: :lipstick: responsive nav

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,73 +1,132 @@
 import Link from 'next/link';
+import { useState } from 'react';
 
-export default function Nav() {
+const Nav = () => {
+  const [navOpen, setNavOpen] = useState(false);
+
   return (
-    <nav className="flex px-5 py-5 font-semibold bg-gray-100 dark:bg-gray-900 shadow-md justify-between items-center">
-      <Link href="/">
-        <a className="text-xl uppercase text-purple-600 dark:text-purple-200 dark:hover:text-purple-400">
-          chris bailey . dev
-        </a>
-      </Link>
-      <div className="flex space-x-4">
-        <div className="nav-item">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            className="nav-icon"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={1.5}
-              d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"
-            />
-          </svg>
-          <Link href="/write">
-            <a className="nav-link">WRITE</a>
-          </Link>
-        </div>
-        <div className="nav-item">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            className="nav-icon"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={1.5}
-              d="M19.428 15.428a2 2 0 00-1.022-.547l-2.387-.477a6 6 0 00-3.86.517l-.318.158a6 6 0 01-3.86.517L6.05 15.21a2 2 0 00-1.806.547M8 4h8l-1 1v5.172a2 2 0 00.586 1.414l5 5c1.26 1.26.367 3.414-1.415 3.414H4.828c-1.782 0-2.674-2.154-1.414-3.414l5-5A2 2 0 009 10.172V5L8 4z"
-            />
-          </svg>
+    <nav className=" px-5 py-5 font-semibold bg-gray-100 dark:bg-gray-900 shadow-md">
+      <div className="flex justify-between items-center">
+        <Link href="/">
+          <a className="text-xl uppercase text-purple-600 dark:text-purple-200 dark:hover:text-purple-400">
+            chris bailey . dev
+          </a>
+        </Link>
 
-          <a href="/make" className="nav-link">
-            MAKE
-          </a>
-        </div>
-        <div className="nav-item">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            className="nav-icon"
+        {/* md+ nav links */}
+        <div className="hidden md:flex space-x-4">{navLinks()}</div>
+
+        {/* hamburger menu hidden md+ */}
+        <div className="md:hidden">
+          <button
+            class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+            aria-expanded="false"
+            onClick={() => setNavOpen(!navOpen)}
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={1.5}
-              d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
-            />
-          </svg>
-          <a href="#" className="nav-link">
-            LIFE
-          </a>
+            <span class="sr-only">Open main menu</span>
+            <svg
+              class="block h-6 w-6"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              aria-hidden="true"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+            </svg>
+            <svg
+              class="hidden h-6 w-6"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              aria-hidden="true"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </button>
         </div>
+      </div>
+
+      {/* small nav links */}
+      <div className={`flex flex-col ${navOpen ? '' : 'hidden'} md:hidden`}>
+        {navLinks()}
       </div>
     </nav>
   );
-}
+};
+
+const navLinks = () => (
+  <>
+    <div className="nav-item">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        className="nav-icon"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={1.5}
+          d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"
+        />
+      </svg>
+      <Link href="/write">
+        <a className="nav-link">WRITE</a>
+      </Link>
+    </div>
+    <div className="nav-item">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        className="nav-icon"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={1.5}
+          d="M19.428 15.428a2 2 0 00-1.022-.547l-2.387-.477a6 6 0 00-3.86.517l-.318.158a6 6 0 01-3.86.517L6.05 15.21a2 2 0 00-1.806.547M8 4h8l-1 1v5.172a2 2 0 00.586 1.414l5 5c1.26 1.26.367 3.414-1.415 3.414H4.828c-1.782 0-2.674-2.154-1.414-3.414l5-5A2 2 0 009 10.172V5L8 4z"
+        />
+      </svg>
+
+      <a href="/make" className="nav-link">
+        MAKE
+      </a>
+    </div>
+    <div className="nav-item">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        className="nav-icon"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={1.5}
+          d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
+        />
+      </svg>
+      <a href="#" className="nav-link">
+        LIFE
+      </a>
+    </div>
+  </>
+);
+
+export default Nav;
