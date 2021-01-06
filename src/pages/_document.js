041dab60ac2c9c2fe2c8c88071b62be40b3a3490
Chris Bailey <chris@ghostly.io>
2021-01-05 22:11:05 -0500

    :poop: (google analytics)

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,10 +5,12 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <link
-            href="https://unpkg.com/prism-theme-night-owl@1.4.0/build/style.css"
-            rel="stylesheet"
-          />
+          <link href="https://unpkg.com/prism-theme-night-owl@1.4.0/build/style.css" rel="stylesheet" />
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-5LHXDSL9TZ"></script>
+          <script>
+            window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments)}
+            gtag('js', new Date()); gtag('config', 'G-5LHXDSL9TZ');
+          </script>
         </Head>
         <body className="bg-gray-50 dark:bg-gray-900">
           <Main />
