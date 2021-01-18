cae501491b39762d2bd9bd90e92de65f2aebbb2c
Chris Bailey <chris@ghostly.io>
2021-01-18 13:13:27 -0500

    :recycle:  emoji favicon

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -7,6 +7,10 @@ export default class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link href="https://unpkg.com/prism-theme-night-owl@1.4.0/build/style.css" rel="stylesheet" />
+          <link
+            rel="icon"
+            href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🚀</text></svg>"
+          />
           <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
           <script
             dangerouslySetInnerHTML={{
