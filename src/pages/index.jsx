3b32ae2785c88499a173a2df3824b4650c868da2
Chris Bailey <chris@ghostly.io>
2021-02-13 23:54:57 -0500

    :bug: (index.jsx) dark mode for new personal blurb

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import { NextSeo } from 'next-seo';
-import  Link  from 'next/link'
+import Link from 'next/link';
 
 import Nav from '../components/nav';
 import PostListItem from '../components/PostListItem';
@@ -22,9 +22,11 @@ export default function IndexPage({ posts }) {
           <div className="mb-2 prose dark:prose-dark prose-purple lg:prose-xl">
             <h1>chris bailey</h1>
           </div>
-          <p className="text-xl">
-            Full stack <strong>web developer</strong> from Michigan with a foundness for
-            <br /> <strong>goldendoodles</strong>, <strong>border collies</strong> and <em>tennis</em>.
+          <p className="text-xl text-black dark:text-white">
+            Full stack <strong className="text-yellow-600 dark:text-yellow-300">web developer</strong> from Michigan
+            with a foundness for
+            <br /> <strong className="text-pink-600 dark:text-pink-300">goldendoodles</strong>,{' '}
+            <strong className="text-pink-600 dark:text-pink-300">border collies</strong> and <em>tennis</em>.
           </p>
         </section>
         <section className="md:m-0">
@@ -35,9 +37,10 @@ export default function IndexPage({ posts }) {
             {posts.map((post, i) => (
               <PostListItem key={i} item={post} />
             ))}
-            <Link href="write"><a className="text-purple-500 underline">more...</a></Link> 
+            <Link href="write">
+              <a className="text-purple-500 underline">more...</a>
+            </Link>
           </div>
-
         </section>
       </div>
     </>
