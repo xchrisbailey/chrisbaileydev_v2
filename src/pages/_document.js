274d8adffa45ef18a8cb847dfab2480177292db9
Chris Bailey <chris@ghostly.io>
2021-03-30 10:15:52 -0400

    add favicon

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -18,7 +18,7 @@ export default class MyDocument extends Document {
 
           <link
             rel="icon"
-            href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🚀</text></svg>"
+            href="/favicon.ico"
           />
           <script
             async
